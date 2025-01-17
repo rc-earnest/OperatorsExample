3c8346cc0b95dc594025f66a4dc5473faf901c33
rc-earnest <rc.earnest04@gmail.com>
2025-01-17 08:19:44 -0700

    add more operator examples

--- OperatorsExample/OperatorsExample.vb
+++ OperatorsExample/OperatorsExample.vb
@@ -7,8 +7,26 @@
 Module OperatorsExample
 
     Sub Main()
-        Console.WriteLine("5 + 3 = ") 'print a string
+
+        'sum operator
+        Console.Write("5 + 3 = ") 'print a string
         Console.WriteLine(5 + 3) 'print the sum
+        Console.WriteLine()
+
+        'difference operator
+        Console.Write("5 - 3 = ") 'print a string
+        Console.WriteLine(5 - 3) 'print the sum
+        Console.WriteLine()
+
+        'product operator
+        Console.Write("5 * 3 = ") 'print a string
+        Console.WriteLine(5 * 3) 'print the sum
+        Console.WriteLine()
+
+        'quotient operator
+        Console.Write("5 / 3 = ") 'print a string
+        Console.WriteLine(5 / 3) 'print the sum
+        Console.WriteLine()
     End Sub
 
 End Module
